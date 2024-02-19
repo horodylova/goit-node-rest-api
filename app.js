1d215930c3ec6e908a7e506673492adb7174bfde
svitlana-horodylova <horodylova.sv@gmail.com>
2024-02-19 20:31:58 +0000

    Made the connection with mongoose
    created env, connected with App

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
-import contactsRouter from "./routes/contactsRouter.js";
+dotenv.config({ path: './envs/development.env' });
 
 const app = express();
+
+const port = process.env.PORT;
+const mongoURI = process.env.MONGO_URL;
+
+mongoose.connect(mongoURI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(port, () => {
+      console.log(`Server is running. Use our API on port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
+
 
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
-
-app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
@@ -21,6 +37,4 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+export default app;
