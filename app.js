7b5215f90af928b3bcc870489e7297452a286a18
svitlana-horodylova <horodylova.sv@gmail.com>
2024-02-26 14:28:04 +0000

    Fixed some mistakes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import 'dotenv/config';
 import './db.js';
+import express from "express"; 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import contactsRouter from "./routes/contactsRouter.js";
@@ -35,5 +35,4 @@ app.use((err, req, res, next) => {
 });
 
 export default app;
-
 
--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
-dotenv.config({ path: "./envs/development.env" });
-
 import mongoose from "mongoose";
+import express from "express"; 
+
+dotenv.config({ path: "./envs/development.env" });
 
 const DB_URL = process.env.MONGO_URL;
 
@@ -9,6 +10,6 @@ mongoose
   .connect(DB_URL)
   .then(() => console.log("Database connected"))
   .catch((error) => {
-    console.error("Error connecting to database:"), error;
+    console.error("Error connecting to database:", error); 
     process.exit(1);
   });
