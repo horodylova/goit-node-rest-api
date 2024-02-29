5c539b1c7e43b533c9f3862e95957b9ffbb30a22
svitlana-horodylova <horodylova.sv@gmail.com>
2024-02-29 17:38:27 +0000

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,9 +3,15 @@ import UserModel from "../models/userModel.js";
 async function register(req, res, next) {
   try {
     const { password, email, subscription, token } = req.body;
-    const result = await UserModel.create({password, email, subscription, token});
-    
-    console.log(result);
+
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      return res.status(400).send('User with this email already exists');
+    }
+
+    const newUser = await UserModel.create({ password, email, subscription, token });
+
+    console.log(newUser);
 
     res.status(201).send('User registered successfully');
   } catch (error) {
@@ -14,3 +20,4 @@ async function register(req, res, next) {
 }
 
 export default { register };
+
